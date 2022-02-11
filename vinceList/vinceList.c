3e1874e6158ff47ba8096328ec1f4d4b9867b254
vince-exe <vincenzocaliendoantonio@gmail.com>
2022-02-11 19:05:29 +0100

    update
    updated .c and .h files

---
+++ vinceList/vinceList.c
@@ -0,0 +1,326 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "vinceList.h"
+
+int appendInt(struct List** head, int value) {
+    /*create a new node*/
+    struct List* new_node = malloc(sizeof(struct List));
+
+    /*check if the malloc failed*/
+    if(!new_node)
+        return 0;
+
+    /*variable used to know the type of the node*/
+    new_node->type = INT;
+        
+    /*assign to the new node the value*/
+    new_node->n_int = value;
+
+    /*connect the new node to the head*/
+    new_node->next = (*head);
+    
+    /*change the position (value) of the head*/
+    (*head) = new_node;
+
+    return 1;
+}
+
+int appendFloat(struct List** head, float value) {
+    /*create a new node*/
+    struct List* new_node = malloc(sizeof(struct List));
+
+    /*check if the malloc failed*/
+    if(!new_node)
+        return 0;
+
+    /*variable used to know the type of the node*/
+    new_node->type = FLOAT;
+        
+    /*assign to the new node the value*/
+    new_node->n_float = value;
+
+    /*connect the new node to the head*/
+    new_node->next = (*head);
+    
+    /*change the position (value) of the head*/
+    (*head) = new_node;
+
+    return 1;
+}
+
+int appendDouble(struct List** head, double value) {
+    /*create a new node*/
+    struct List* new_node = malloc(sizeof(struct List));
+
+    /*check if the malloc failed*/
+    if(!new_node)
+        return 0;
+
+    /*variable used to know the type of the node*/
+    new_node->type = DOUBLE;
+            
+    /*assign to the new node the value*/
+    new_node->n_double = value;
+
+    /*connect the new node to the head*/
+    new_node->next = (*head);
+    
+    /*change the position (value) of the head*/
+    (*head) = new_node;
+
+    return 1;   
+}
+
+int appendChar(struct List** head, char c) {
+    /*create a new node*/
+    struct List* new_node = malloc(sizeof(struct List));
+
+    /*check if the malloc falied*/
+    if(!new_node)
+        return 0;
+    
+    /*variable used to know the type of the node*/
+    new_node->type = CHAR;
+
+    /*assign to the new node the value*/
+    new_node->c = c;
+
+    /*connect the new node to the head*/
+    new_node->next = (*head);
+
+    /*change the position (value) of the head*/
+    (*head) = new_node;
+
+    return 1;
+}
+
+void printList(struct List* head) {
+    /*while head != NULL*/
+    while(head) {
+        
+        switch (head->type) {
+        /*if the value to print is an integer*/
+        case INT:
+            printf("%d\n", head->n_int);
+            break;
+
+        /*if the value to print is a float*/
+        case FLOAT:
+            printf("%f\n", head->n_float);
+            break;
+
+        /*if the value to print is a double*/
+        case DOUBLE:
+            printf("%lf\n", head->n_double);
+            break;
+
+        /*if the value to print is a char*/
+        case CHAR:
+            printf("%c\n", head->c);
+
+        default:
+            break;
+        }
+        /*move to the next node*/
+        head = head->next;                                                                                                                                             
+    }   
+}
+
+int isInteger(struct List* head, int key) {
+    /*while head != NULL*/
+    while(head) {
+        /*check if the data exist*/
+        if(head->n_int == key)
+            return 1;
+        head = head->next;
+    }
+    return 0;
+}
+
+int isFloat(struct List* head, float key) {
+    /*while head != NULL*/
+    while(head) {
+        /*check if the data exist*/
+        if(head->n_float == key)
+            return 1;
+        head = head->next;
+    }
+    return 0;
+}
+
+int isDouble(struct List* head, double key) {
+    /*while head != NULL*/
+    while(head) {
+        /*check if the data exist*/
+        if(head->n_double == key)
+            return 1;
+        head = head->next;
+    }
+    return 0;
+}
+
+int isChar(struct List* head, char key) {
+    /*while head != NULL*/
+    while(head) {
+        /*check if the data exist*/
+        if(head->c == key)
+            return 1;
+        head = head->next;
+    }
+    return 0;
+}
+
+int removeInt(struct List** head, int key) {
+    /*save the value of the head in a temp pointer*/
+    struct List* temp = (*head);
+    /*pointer used to point to the previus nodes*/
+    struct List* prev = NULL;
+
+    /*if the value to delete is the head*/
+    if(temp->n_int == key) {
+        /*move the head*/
+        (*head) = temp->next;
+        /*free the value*/
+        free(temp);
+
+        return 1;
+    } 
+
+    /*while temp != NULL and temp->int != key*/
+    while(temp && temp->n_int != key) {
+        /*assign the value of temp to prev*/
+        prev = temp;
+        /*move temp to the next node*/
+        temp = temp->next;
+    }
+
+    /*if the key doesn't exist*/
+    if(!temp)
+    return 0;
+
+    /*else connect the prev node to the next of temp*/
+    prev->next = temp->next;
+    /*free temp*/
+    free(temp);
+
+    return 1;
+}
+
+int removeFloat(struct List** head, float key) {
+    /*save the value of the head in a temp pointer*/
+    struct List* temp = (*head);
+    /*pointer used to point to the previus nodes*/
+    struct List* prev = NULL;
+
+    /*if the value to delete is the head*/
+    if(temp->n_float == key) {
+        /*move the head*/
+        (*head) = temp->next;
+        /*free the value*/
+        free(temp);
+
+        return 1;
+    } 
+
+    /*while temp != NULL and temp->float != key*/
+    while(temp && temp->n_float != key) {
+        /*assign the value of temp to prev*/
+        prev = temp;
+        /*move temp to the next node*/
+        temp = temp->next;
+    }
+
+    /*if the key doesn't exist*/
+    if(!temp)
+    return 0;
+
+    /*else connect the prev node to the next of temp*/
+    prev->next = temp->next;
+    /*free temp*/
+    free(temp);
+
+    return 1;
+}
+
+int removeDouble(struct List** head, double key) {
+    /*save the value of the head in a temp pointer*/
+    struct List* temp = (*head);
+    /*pointer used to point to the previus nodes*/
+    struct List* prev = NULL;
+
+    /*if the value to delete is the head*/
+    if(temp->n_double == key) {
+        /*move the head*/
+        (*head) = temp->next;
+        /*free the value*/
+        free(temp);
+
+        return 1;
+    } 
+
+    /*while temp != NULL and temp->double != key*/
+    while(temp && temp->n_double != key) {
+        /*assign the value of temp to prev*/
+        prev = temp;
+        /*move temp to the next node*/
+        temp = temp->next;
+    }
+
+    /*if the key doesn't exist*/
+    if(!temp)
+    return 0;
+
+    /*else connect the prev node to the next of temp*/
+    prev->next = temp->next;
+    /*free temp*/
+    free(temp);
+
+    return 1;
+}
+
+int removeChar(struct List** head, char key) {
+    /*save the value of the head in a temp pointer*/
+    struct List* temp = (*head);
+    /*pointer used to point to the previus nodes*/
+    struct List* prev = NULL;
+
+    /*if the value to delete is the head*/
+    if(temp->c == key) {
+        /*move the head*/
+        (*head) = temp->next;
+        /*free the value*/
+        free(temp);
+
+        return 1;
+    } 
+
+    /*while temp != NULL and temp->char != key*/
+    while(temp && temp->c != key) {
+        /*assign the value of temp to prev*/
+        prev = temp;
+        /*move temp to the next node*/
+        temp = temp->next;
+    }
+
+    /*if the key doesn't exist*/
+    if(!temp)
+    return 0;
+
+    /*else connect the prev node to the next of temp*/
+    prev->next = temp->next;
+    /*free temp*/
+    free(temp);
+
+    return 1;
+}
+
+void freeList(struct List** head) {
+    /*while head != NULL*/
+    while((*head)) {
+        /*free the node*/
+        free((*head));
+        /*move to the next node*/
+        (*head) = (*head)->next;
+    }
+}
---
+++ vinceList/vinceList.h
@@ -0,0 +1,82 @@
+#ifndef VINCELIST_H
+#define VINCELIST_H
+
+#define INT 1
+#define FLOAT 2
+#define DOUBLE 3
+#define CHAR 4
+
+/*
+Library created for the manipulation of the lists in C
+
+Tested on linux Ubuntu with valgrind program
+
+What's valgrind?
+
+Info = [https://valgrind.org/]
+
+Founder: Caliendo Vincenzo aka vince-exe
+
+GitHub: [https://github.com/vince-exe]
+*/
+
+typedef struct List {
+    float n_float;
+    int n_int;
+    double n_double;
+    char c;
+    /*variable to know of what type is the node
+      1 = integer
+      2 = float
+      3 = double
+      4 = char
+    */
+    short int type;
+      
+    /*pointers to others nodes of the list*/
+    struct List* next;
+} List;
+
+/*function that append an integer to the list*/
+int appendInt(struct List** head, int value);
+
+/*function that append a float to the list*/
+int appendFloat(struct List** head, float value);
+
+/*function that append a double to the list*/
+int appendDouble(struct List** head, double value);
+
+/*function that append a char to the list*/
+int appendChar(struct List** head, char c);
+
+/*function to print the list*/
+void printList(struct List* head);
+
+/*function that return 1 if the data (integer) is in the list, else return 0*/
+int isInteger(struct List* head, int key);
+
+/*function that return 1 if the data (float) is in the list, else return 0*/
+int isFloat(struct List* head, float key);
+
+/*function that return 1 if the data (double) is in the list, else return 0*/
+int isDouble(struct List* head, double key);
+
+/*function that return 1 if the data (char) is in the list, else return 0*/
+int isChar(struct List* head, char key);
+
+/*function to delete a node by key in the list (int)*/
+int removeInt(struct List** head, int key);
+
+/*function to delete a node by key in the list (float)*/
+int removeFloat(struct List** head, float key);
+
+/*function to delete a node by key in the list (double)*/
+int removeDouble(struct List** head, double key);
+
+/*function to delete a node by key in the list (char)*/
+int removeChar(struct List** head, char key);
+
+/*function that free all nodes of the list*/
+void freeList(struct List** head);
+
+#endif
